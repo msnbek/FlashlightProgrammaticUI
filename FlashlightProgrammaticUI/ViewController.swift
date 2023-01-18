f8d09cdabfebf8524be062d3a062ac3dd0f50d94
msnbek <mahmutsenbek@icloud.com>
2023-01-18 14:49:27 +0300

    Initial Commit

---
+++ FlashlightProgrammaticUI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlashlightProgrammaticUI
+//
+//  Created by Mahmut Senbek on 18.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
